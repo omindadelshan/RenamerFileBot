f7ebe275c6b056234f7c98a8519e60a756e7bdac
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-04-07 10:13:34 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,25 @@
+class script(object):
+    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail upport!💯
+
+<b>Send me any Telegram file and choose appropriate option! </b>
+
+<b>Made With ❤️ By @ExploitzBots"""
+
+    RENAME_403_ERR = "What Are You Doing Gay 😒? You Will Be Banned"
+    UPGRADE_TEXT = "Support : @Madboi_xD"
+    DOWNLOAD_START = "Give Me Some Time😌..."
+    UPLOAD_START = """ Starting To Upload....
+  
+    Join @ExploitzBots ❤️"""
+    
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > @ExploitzBots**"
+    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+    DEL_THUMB = "Thumbnail cleared succesfully!"
+    NO_THUMB = "No thumbnails found!"
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    CUSTOM_CAPTION_UL_FILE = " "
+    HELP_USER = """It's not that complicated😅
+    
+1. Send me any Telegram File.
+2. Choose appropriate option."""
+
