283603988eaaf09225d96366e8f892450479aded
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-18 17:42:12 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,25 +1,26 @@
 class script(object):
     START_TEXT = """<b>🌟Hello I Am A Simple File Renamer Bot With Permanent Thumbnail upport🔧🔧.. I Can Rdname Your File So Esy❤️.¡
 
-<b>💎Send me any Telegram file and choose appropriate option💎! </b>
+<b>💎Send me any Telegram file and choose appropriate option💎 😇 Get A More Help Send👉 /help 👈command🌟..! </b>
 
 <b🔧 This bot Made By >Made With ❤️ By @omindas"""
 
     RENAME_403_ERR = "What Are You Doing Gay 😒? You Will Be Banned"
     UPGRADE_TEXT = "Support : @sdprojectupdates"
-    DOWNLOAD_START = "Give Me Some Time😌..."
-    UPLOAD_START = """ Starting To Upload....
+    DOWNLOAD_START = "Please Give Me A Some Time ⏲️...."
+    UPLOAD_START = """ 📤Starting To Upload📤....
   
-    👨‍💻 Get A SupportA Join @sdbotworld ❤️"""
+    👨‍💻 Get A Support A Join @sdbotworld ❤️"""
     
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > @SD_File_Renamer_Bot**"
-    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
-    DEL_THUMB = "Thumbnail cleared succesfully!"
-    NO_THUMB = "No thumbnails found!"
+    SAVED_THUMB = "✅ Thumbnail Saved Permently ✅"
+    DEL_THUMB = "✳️Thumbnail cleared succesfully✳️!"
+    NO_THUMB = "😆No thumbnails found😆!"
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
     CUSTOM_CAPTION_UL_FILE = " "
     HELP_USER = """It's not that complicated😅
     
 1. Send me any Telegram File.
-2. Choose appropriate option."""
+2. Choose appropriate option.
+😋This Bot Made By @Omindas ❤️🤔"""
 
